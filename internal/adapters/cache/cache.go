70c8eb78b86bb3fcd60b757ebf5cbe56c560d521
shalimski <shalimski@gmail.com>
2022-09-26 15:13:06 +0800

    switch redis to interface

--- internal/adapters/cache/cache.go
+++ internal/adapters/cache/cache.go
@@ -7,13 +7,15 @@ import (
 	"github.com/go-redis/redis/v9"
 	"github.com/shalimski/shortener/config"
 	"github.com/shalimski/shortener/internal/domain"
+	"github.com/shalimski/shortener/internal/ports"
 )
 
 type cache struct {
 	rdb *redis.Client
 }
 
-func NewCache(cfg *config.Config) *cache {
+// NewCache create instance of redis server
+func NewCache(cfg *config.Config) ports.Cacher {
 	return &cache{
 		rdb: redis.NewClient(&redis.Options{
 			Addr:     cfg.Redis.DSN,
@@ -23,10 +25,12 @@ func NewCache(cfg *config.Config) *cache {
 	}
 }
 
+// Set value by key
 func (c *cache) Set(ctx context.Context, key, value string) error {
 	return c.rdb.Set(ctx, key, value, 0).Err()
 }
 
+// Get value by key
 func (c *cache) Get(ctx context.Context, key string) (string, error) {
 	url, err := c.rdb.Get(ctx, key).Result()
 	if errors.Is(err, redis.Nil) {
@@ -40,6 +44,7 @@ func (c *cache) Get(ctx context.Context, key string) (string, error) {
 	return url, nil
 }
 
+// Delete value by key
 func (c *cache) Del(ctx context.Context, key string) error {
 	return c.rdb.Del(ctx, key).Err()
 }
